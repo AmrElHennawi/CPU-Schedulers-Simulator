409b19d920404337690623905b2bcc452b0a6320
abwael <11410120210490@stud.cu.edu.eg>
2023-12-14 23:16:08 +0200

    SJF Schedular

--- Scheduler.java
+++ Scheduler.java
@@ -159,12 +159,49 @@ class SJFScheduler extends Scheduler{
 
     @Override
     public void schedule() {
+        // Sort processes based on burst time then the arrival time
+        Collections.sort(processes, Comparator.comparingInt((Process p) -> p.getBurstTime()).thenComparingInt(p -> p.getArrivalTime()));
+
+        int currentTime = 0;
+
+        // Calculate waiting and turnaround time for each process
+        for (Process process : processes) {
+            if (process.getArrivalTime() > currentTime) {
+                currentTime = process.getArrivalTime();
+            }
+
+            process.setWaitingTime(currentTime - process.getArrivalTime());
+            process.setTurnaroundTime(process.getWaitingTime() + process.getBurstTime());
+
+            currentTime += process.getBurstTime();
+            executionOrder.add(process.getProcessName());
+        }
         
     }
 
     @Override
     public List<Process>getInput() {
-        return processes;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the number of processes: ");
+            int numProcesses = scanner.nextInt();
+
+            List<Process> processes = new ArrayList<>();
+
+            // Input parameters for each process
+            for (int i = 0; i < numProcesses; i++) {
+                System.out.println("Process " + (i + 1) + ":");
+                System.out.print("Name: ");
+                String name = scanner.next();
+                System.out.print("Arrival Time: ");
+                int arrivalTime = scanner.nextInt();
+                System.out.print("Burst Time: ");
+                int burstTime = scanner.nextInt();
+                this.setNumProcesses(numProcesses);
+                Process process = new Process(name, arrivalTime, burstTime, 0);
+                processes.add(process);
+            }
+            return processes;
+        }
     }
     
 }
