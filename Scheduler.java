9d7695ae296f68f4e79c759e96d36f4a35e8c3a6
abwael <11410120210490@stud.cu.edu.eg>
2023-12-15 17:42:29 +0200

    Priority Edit

--- Scheduler.java
+++ Scheduler.java
@@ -156,6 +156,35 @@ class PriorityScheduler extends Scheduler {
         Collections.sort(processes,
                 Comparator.comparingInt((Process p) -> p.getArrivalTime()).thenComparingInt(p -> p.getPriorityNum()));
 
+        int end = processes.get(0).getArrivalTime();
+
+        //this loop should sort the processes to the final order they will be executed on
+        //current
+        for(int i = 0; i < processes.size()-1;i++){
+            int start;
+            if(processes.get(i).getArrivalTime() <= end){
+                start = end;
+            }
+            else{
+                start = processes.get(i).getArrivalTime();
+            }
+
+            end = start + processes.get(i).getBurstTime();
+            int minIndex = i+1;
+
+            //search next
+            for(int j = i+1; j < processes.size(); j++){
+                if((processes.get(j).getArrivalTime() < start) ||
+                        (processes.get(j).getArrivalTime() >= start && processes.get(j).getArrivalTime() < end)){
+                    if(processes.get(j).getPriorityNum() < processes.get(minIndex).getPriorityNum()){
+                        minIndex = j;
+                    }
+                }
+            }
+
+            Collections.swap(processes,i+1,minIndex);
+        }
+
         int currentTime = 0;
 
         // Calculate waiting time and turnaround time for each process
@@ -206,7 +235,8 @@ class SJFScheduler extends Scheduler {
     @Override
     public void schedule() {
         // Sort processes based on burst time then the arrival time
-        Collections.sort(processes, Comparator.comparingInt((Process p) -> p.getArrivalTime()).thenComparingInt(p -> p.getBurstTime()));
+        Collections.sort(processes,
+                Comparator.comparingInt((Process p) -> p.getArrivalTime()).thenComparingInt(p -> p.getBurstTime()));
 
 
         int end = processes.get(0).getArrivalTime();
@@ -227,7 +257,8 @@ class SJFScheduler extends Scheduler {
 
             //search next
             for(int j = i+1; j < processes.size(); j++){
-                if((processes.get(j).getArrivalTime() < start) || (processes.get(j).getArrivalTime() >= start && processes.get(j).getArrivalTime() < end)){
+                if((processes.get(j).getArrivalTime() < start) ||
+                        (processes.get(j).getArrivalTime() >= start && processes.get(j).getArrivalTime() < end)){
                     if(processes.get(j).getBurstTime() < processes.get(minIndex).getBurstTime()){
                         minIndex = j;
                     }
